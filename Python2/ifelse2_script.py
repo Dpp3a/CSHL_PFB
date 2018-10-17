45de78f192b4d95f2a812bb6a3840746f28d4881
spa <spa@pfb16.cshl.edu>
2018-10-17 12:13:02 -0400

    python3 scripts

--- Python2/ifelse2_script.py
+++ Python2/ifelse2_script.py
@@ -5,15 +5,15 @@ my_number = int(sys.argv[1])
 
 if my_number > 0:
   if my_number < 50:
-      if my_number%2==0:
-        print('Number is positive, even and smaller than 50')
-      else:
-        print('Number is positive, uneven and smaller than 50')
+    if my_number%2==0:
+      print('Number is positive, even and smaller than 50')
+    else:
+      print('Number is positive, uneven and smaller than 50')
   else:
     if my_number%3==0:
       print('Number is positive, larger than 50 and divisible by 3')
     else:
-      print('Number is positive and larger than 50')
+      print('Number is pisivte and larger than 50')
 elif my_number == 0:
   print('Number is 0')
 else:
