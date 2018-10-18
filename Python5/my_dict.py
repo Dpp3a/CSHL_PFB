c0d07826187fa7e1fc341ec8c276f21eba31c6a2
spa <spa@pfb16.cshl.edu>
2018-10-17 21:19:27 -0400

    dict scripts

---
+++ Python5/my_dict.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+#script in which you construct a dictionary of your favorite things
+fav_dict = {'book' : 'Artemis', 'song' : 'I still remember - Bloc Party', 'tree' : 'Cedar'}
+
+#Print out your favorite book
+print(fav_dict['book']) 
+
+#Print out your favorite book but use a variable in the key.
+fav_thing = 'book'
+print(fav_dict[fav_thing])
+
+#Add your favorite 'organism' to the dictionary.
+fav_dict['organism'] = 'guinea pig'
+
+fav_thing = 'organism'
+print(fav_dict[fav_thing])
+print('end of command')
+
+#Take a value from the command line for fav_thing and print the value of that item from the dictionary.
+import sys
+ = sys.argv(1)
+
+
+#Change the value of your favorite organism.
+fav_dict['organism'] = 'rabbit'
+print(fav_dict) 
+
+#Get the fav_thing from the command line and a new value for that key. Change the value with the user inputted value.
+
+
+#Use a for loop to print out each key and value of the dictionary.
+
+
+#Write a script to find the intersection, difference, union, and symetrical difference between these two sets.
+
