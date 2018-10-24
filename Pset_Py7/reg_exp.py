f87dd52b01331ac2eb43a2ff6e806d69e7117552
spa <spa@pfb16.cshl.edu>
2018-10-23 20:13:49 -0400

    add scripts

--- Pset_Py7/reg_exp.py
+++ Pset_Py7/reg_exp.py
@@ -15,10 +15,22 @@ with open('Python_07_nobody.txt', 'r') as file_object, open('Python_07_klaus.txt
         print(klaus)
         file_object2.write(klaus)
 print('End of nobody code')
-        
-#Using pattern matching, find all the header lines in Python_07.fasta.
+
+#Using pattern matching, find all the header lines in Python_07.fasta. If a line matches the format of a FASTA header, extract the sequence name and description using sub patterns
+#Solution1:
 with open('Python_07.fasta', 'r') as file_object:
     for line in file_object:
         match = re.search(r'^>(\S+)\s(.*)', line)
         if match:
             print('ID: {}\tDES: {}'.format(match.group(1), match.group(2)))
+
+#Solution2:
+fasta_open = open('Python_07.fasta', r)
+
+for line in fasta_open:
+    line = line.rstrip()
+    if line.startswith('>'):
+        for (seq_id, desc) in re.findall(r'>(\S+)\s(.*)):
+            print('ID: ', seq_id, 'Description: ', desc)
+
+fasta_open.close()
